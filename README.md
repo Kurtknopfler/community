061f3a8a9f86d14c2b185f98817c3ce2e371a6b8
blackzz <zzhendrix@gmail.com>
2020-08-10 13:45:46 +0800

    add login model and extract github config from code to properties

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 ## 第一个 Springboot Demo —— 社区
 
 ## 资料
-https://spring.io/guides/gs/serving-web-content/
+[Spring Web](https://spring.io/guides/gs/serving-web-content/)
 
-https://spring.io/guides
+[Spring 文档](https://spring.io/guides)
 
-https://elasticsearch.cn/explore
- 
-https://docs.github.com/en/developers/overview/managing-deploy-keys#deploy-keys
+[elasticsearch](https://elasticsearch.cn/explore)
 
+[Github deploy key](https://docs.github.com/en/developers/overview/managing-deploy-keys#deploy-keys)
+
+[Bootstrap](https://v3.bootcss.com/getting-started/)
+
+[Gihub OAuth](https://docs.github.com/en/developers/apps/creating-an-oauth-app)
 
 ## 工具
-https://git-scm.com/downloads
+[Git](https://git-scm.com/downloads)
 
-https://maven.apache.org/download.cgi
+[Maven](https://maven.apache.org/download.cgi)
 
 
 
---
+++ src/main/java/zz/black/community/community/controller/AuthorizeController.java
@@ -0,0 +1,47 @@
+package zz.black.community.community.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import zz.black.community.community.dto.AccessTokenDTO;
+import zz.black.community.community.dto.GithubUser;
+import zz.black.community.community.provider.GithubProvider;
+
+/**
+ *
+ */
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String reDirectUri;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state)
+            //通过 @requestparam 获取传回的参数
+    {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri(reDirectUri);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getName());
+
+        return "index";
+    }
+}
